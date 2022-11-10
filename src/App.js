c499d2599b76e1221a196ce1c7e68e76c5cecbc6
happyGGang <joy.gang@dxworks.kr>
2022-11-10 23:10:38 +0900

    [update] 상단바 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import { Button } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Button variant="primary">Primary</Button>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">shoeShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#feature">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 }
