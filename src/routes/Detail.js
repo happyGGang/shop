845e6840869d2b8aa595f96e94f4c8c9af5dde25
happyGGang <joy.gang@dxworks.kr>
2022-11-14 23:42:05 +0900

    [update] 버튼추가

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,10 +1,26 @@
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+let Box = styled.div`
+  padding: 20px;
+  color: grey;
+`;
+let YellowBtn = styled.button`
+  background: yellow;
+  color: black;
+  padding: 10px;
+`;
 
 function Detail(props) {
   let { id } = useParams();
 
   return (
     <div className="container">
+      <div>
+        <Box>
+          <YellowBtn>버튼임</YellowBtn>
+        </Box>
+      </div>
       <div className="row">
         <div className="col-md-6">
           <img
